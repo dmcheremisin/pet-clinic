89ee29af00fe1ffdd5e152bd42cfcadb11129f62
dmitrii89 <dmitry_cher@mail.ru>
2019-07-17 19:32:55 +0300

    initial commit

---
+++ src/main/java/info/cheremisin/petclinic/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package info.cheremisin.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
