fc082f924f8e586422f3176e841ac8490d6cdf0f
dmitrii89 <dmitry_cher@mail.ru>
2019-07-24 12:27:21 +0300

    List all Vets on Vet index page. Closes #15

--- pet-clinic-web/src/main/java/info/cheremisin/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/info/cheremisin/petclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package info.cheremisin.petclinic.controllers;
 
+import info.cheremisin.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOfVets() {
+    public String listOfVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
