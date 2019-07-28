05bd4d65a67fb88430be587bf95f60a6174e8fee
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-07-28 22:47:58 +0300

    Create PetType Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/info/cheremisin/petclinic/service/PetTypeService.java
@@ -0,0 +1,10 @@
+package info.cheremisin.petclinic.service;
+
+import info.cheremisin.petclinic.model.PetType;
+
+/**
+ * Created by Dmitrii on 28.07.2019.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/info/cheremisin/petclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,39 @@
+package info.cheremisin.petclinic.service.map;
+
+import info.cheremisin.petclinic.model.PetType;
+import info.cheremisin.petclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Dmitrii on 28.07.2019.
+ */
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
