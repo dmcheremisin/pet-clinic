be62deada4e982fc06404ef4cdd7b7d2b3c9533f
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-07-28 22:55:45 +0300

    Add PetTypes in with Bootstrap (startup). Closes #32

--- pet-clinic-web/src/main/java/info/cheremisin/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/info/cheremisin/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package info.cheremisin.petclinic.bootstrap;
 
 import info.cheremisin.petclinic.model.Owner;
+import info.cheremisin.petclinic.model.PetType;
 import info.cheremisin.petclinic.model.Vet;
 import info.cheremisin.petclinic.service.OwnerService;
+import info.cheremisin.petclinic.service.PetTypeService;
 import info.cheremisin.petclinic.service.VetService;
 import info.cheremisin.petclinic.service.map.OwnerMapService;
 import info.cheremisin.petclinic.service.map.VetMapService;
@@ -14,14 +16,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
