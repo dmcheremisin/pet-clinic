a2c45e82728098cef1e57ab0de2fa44ab85284fb
dmitrii89 <dmitry_cher@mail.ru>
2019-08-06 21:15:20 +0300

    Convert Vets to JPA Entities. Closes #37

--- pet-clinic-data/src/main/java/info/cheremisin/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/info/cheremisin/petclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package info.cheremisin.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Dmitrii on 28.07.2019.
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
