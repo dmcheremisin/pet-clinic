f5d250ea6595595a93425099d63aef4efaae02f4
dmitrii89 <dmitry_cher@mail.ru>
2019-08-06 21:21:07 +0300

    Create Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/info/cheremisin/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/info/cheremisin/petclinic/model/Visit.java
@@ -1,14 +1,28 @@
 package info.cheremisin.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 /**
  * Created by Dmitrii on 28.07.2019.
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
